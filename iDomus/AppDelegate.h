04ac82dd399449944dd5af3e3142850541742fc4
Giuseppe Acito <giuseppe.acito@gmail.com>
2011-11-09 19:58:05 +0100

    Initial Commit

---
+++ iDomus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iDomus
+//
+//  Created by Giuseppe Acito on 09/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
